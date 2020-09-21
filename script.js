9b284dbe1eefa5c35a67f27e79d3937e8124d841
Tyler Hall <tylerhall12@outlook.com>
2020-09-21 21:04:45 +1000

    Code comments and console logs for error detection

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var counter = 0;
 
 // Function to assign a random position for the hole in each pole animation
 hole.addEventListener('animationiteration', () => {
-    var random = -((Math.random() * 400) + 200);
+    var random = -((Math.random() * 400) + 200);  // Originally 400 & 200
     hole.style.top = random + "px";
     counter++;
 })
@@ -15,15 +15,24 @@ hole.addEventListener('animationiteration', () => {
 // Gravity function (setInterval)
 setInterval(function(){
     let beeTop = parseInt(window.getComputedStyle(bee).getPropertyValue('top'));
+
     if(jump == 0) {
         bee.style.top = (beeTop + 3) + "px";
     }
-    var pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue('left'));
-    console.log(`PipeLeft is ${pipeLeft}`);
 
+    var pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue('left'));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue('top'));
     var bTop = -(741 - beeTop);
-    if((beeTop > 695) || ((pipeLeft < 217) && (pipeLeft > 147) && ((bTop < holeTop) || (bTop > holeTop + 150)))){
+
+    if((beeTop > 695) || ((pipeLeft < 217) && (pipeLeft > 147) && ((bTop < holeTop) || (bTop > holeTop + 110)))){
+
+        // Use these to trouble shoot the conditional statements above to find out which is failing
+        console.log(`beeTop > 695: ${beeTop > 695}`)
+        console.log(`pipeLeft < 217: ${pipeLeft < 217}`);
+        console.log(`pipeLeft > 147: ${pipeLeft > 147}`);
+        console.log(`beeTop < holeTop: ${beeTop < holeTop}`);
+        console.log(`beeTop > holeTop + 110: ${beeTop > holeTop + 110}`);
+        
         alert('Game Over. Score: ' + (counter -1));
         bee.style.top = 330 + 'px';
         counter = 0;
@@ -53,6 +62,14 @@ function jumping() {
         jumpCount++;
     },10)
 }
+
+// Function to control jumping with spacebar
+function spacebar(e) {
+    if (e.keyCode === 32) {
+        jumping()
+    }
+}
+document.addEventListener('keyup', spacebar)
 
 
 
